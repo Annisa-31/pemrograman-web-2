5ba1e471a2c349709a22322eadfc50cdc4a06288
Annisa-31 <Thewatoway31@gmail.com>
2026-09-17 19:39:49 +0700

    Update latihan4.php

--- pertemuan4/latihan4.php
+++ pertemuan4/latihan4.php
@@ -1,12 +1,6 @@
 <?php
-
-for ($i = 1; $i <= 10; $i++) {
-
-    if ($i % 2 == 0) {
-        continue;
-    }
-
-    echo $i . "<br>";
-}
-
+    for($i = 1 ;$i < 11 ;$i++) {
+        if ($i % 2 == 0 ) continue ;
+        else echo $i ;
+        }
 ?>
