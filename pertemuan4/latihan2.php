b06c6fa6c6b10c2f61aa410890fd8ddd22ca8ca7
Annisa-31 <Thewatoway31@gmail.com>
2026-09-17 19:29:10 +0700

    Refactor do-while loop output in latihan2.php
    Updated output messages and variable increment syntax.

--- pertemuan4/latihan2.php
+++ pertemuan4/latihan2.php
@@ -1,14 +1,12 @@
 <?php
 
 $i = 0;
-
-echo "This code will run at least once because i default value is 0.<br><br>";
+echo 'This code will run at least once because i default value is 0.<br>';
 
 do {
-
-    echo "Nilai i = $i <br>";
-    $i++;
-
-} while ($i < 10);
+    echo 'i value is ' . $i . ', so code block will run. <br>';
+++$i;
+} 
+while ($i < 10);
 
 ?>
